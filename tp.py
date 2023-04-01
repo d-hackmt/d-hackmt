155b8d51cbdaaf578227f35f1f85bcaf0606e02a
Divesh Jadhwani <113240252+d-hackmt@users.noreply.github.com>
2023-04-01 16:43:35 +0530

    Delete tp.py

--- tp.py
+++
@@ -1,9 +0,0 @@
-lst = [5,1,"Hi",2,"Hi"]
-ans = list(set(lst))
-print(ans)
-def remove_duplicates():
-    t = ['a', 'b', 'c', 'd']
-    t2 = ['a', 'c', 'd']
-    for t in t2:
-        t.append(t.remove())
-    return t
